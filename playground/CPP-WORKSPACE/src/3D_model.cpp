c94c8034fac93385ffdb9afccabc6921d8b43623
leandro-svg <leandro.dibella@gmail.com>
2022-11-24 15:32:48 +0100

    3D world

--- playground/CPP-WORKSPACE/src/3D_model.cpp
+++ playground/CPP-WORKSPACE/src/3D_model.cpp
@@ -38,7 +38,10 @@ int main()
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
-    
+    if( !glfwInit() ) {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
