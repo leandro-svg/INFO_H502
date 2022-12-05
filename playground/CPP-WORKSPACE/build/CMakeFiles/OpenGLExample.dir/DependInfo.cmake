12e87c33c2f3ba2d03bd9d14fdcf72d208806278
leandro-svg <leandro.dibella@gmail.com>
2022-12-05 15:15:31 +0100

    3D world

---
+++ playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/glad.c" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "src"
+  "../src"
+  "../include"
+  "../include/glad"
+  "../include/GLFW"
+  "../include/json"
+  "../include/KHR"
+  "../include/stb"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/Camera.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/Camera.cpp.o"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/EBO.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/EBO.cpp.o"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/Shader.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/Shader.cpp.o"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/VAO.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/VAO.cpp.o"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/VBO.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/VBO.cpp.o"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/main.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/main.cpp.o"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/src/texture.cpp" "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/OpenGLExample.dir/src/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "../src"
+  "../include"
+  "../include/glad"
+  "../include/GLFW"
+  "../include/json"
+  "../include/KHR"
+  "../include/stb"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/GLAD.dir/DependInfo.cmake"
+  "/home/leand/ULB_course/INFO-H502/playground/CPP-WORKSPACE/build/CMakeFiles/STB_IMAGE.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
